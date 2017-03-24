9e9dc82f298c92a251068b92f15b8d414d969aa9
张伟 <zhangwei19890518@gmail.com>
2017-03-24 20:56:50 +0800

    complete the homework

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,16 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if current_user != @review.user
+      redirect_to account_reviews_path, alert: "你没有编辑评论权限！"
+    else
+      render :edit
+    end
+  end
+
   def create
     @movie = Movie.find(params[:movie_id])
     @review = Review.new(review_params)
@@ -16,6 +26,30 @@ class ReviewsController < ApplicationController
       redirect_to movie_path(@movie)
     else
       render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if current_user != @review.user
+      redirect_to account_reviews_path, alert: "你没有编辑评论权限！"
+    end
+    if @review.update(review_params)
+      redirect_to account_reviews_path, alert: "编辑评论成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if current_user != @review.user
+      redirect_to account_reviews_path, alert: "你没有删除评论权限！"
+    else
+      @review.destroy
+      redirect_to account_reviews_path, alert: "删除评论成功！"
     end
   end
 
